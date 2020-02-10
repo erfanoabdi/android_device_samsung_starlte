2ac93fc6f6c0326e31600de5d33085570fa15dd9
Erfan Abdi <erfangplus@gmail.com>
2020-02-10 13:04:40 +0330

    starlte: Convert tree to build vendor
    * Based on https://github.com/exynos8895/android_device_samsung_dreamlte
    
    Change-Id: I1110986e8a9bcb0b7bba54ebef4daf200d05bace

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,9 +10,6 @@ TARGET_OTA_ASSERT_DEVICE := starltexx,starlteks,starlte
 TARGET_KERNEL_CONFIG := exynos9810-starlte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/universal9810-star
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
-
 # Inherit common board flags
 include device/samsung/universal9810-common/BoardConfigCommon.mk
 
--- device.mk
+++ device.mk
@@ -3,8 +3,8 @@
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Property overrides
--include $(LOCAL_PATH)/system_prop.mk
-
 # Inherit common device configuration
 $(call inherit-product, device/samsung/universal9810-common/universal9810-common.mk)
+
+# Inherit device vendor
+$(call inherit-product-if-exists, vendor/samsung/starlte/starlte-vendor.mk)
